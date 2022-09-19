2297480b8975600ec9bcc34f64e5943b2cbf2abb
praveenhiremath <44702459+praveenhiremath@users.noreply.github.com>
2022-09-19 17:51:32 +0200

    Create elk_to_potfit.py

---
+++ Iron/convert_elk_to_potfit/elk_to_potfit.py
@@ -0,0 +1,2 @@
+# Author: Praveenkumar Hiremath, TU Bergakademie Freiberg, Sachsen, Germany (Postleitzahl: 09599). May, 2016. 
+# This script collects output data from elk runs and converts it to input configuration file format of potfit package.
